b4aec65ed38ad1674a24bf790b84affeeac005a3
Ruochun Zhang <rzhang294@euler-login-2.wacc.wisc.edu>
2022-09-01 00:04:45 -0500

    Add first script: viper

---
+++ projects/core_tests/test_DEM_Viper.cpp
@@ -0,0 +1,141 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2021 projectchrono.org
+// All right reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: RZ
+// =============================================================================
+//
+// Demo to show Viper Rover operated on SCM Terrain
+//
+// =============================================================================
+
+#include "chrono_models/robot/viper/Viper.h"
+
+#include "chrono/physics/ChSystemSMC.h"
+#include "chrono/physics/ChBodyEasy.h"
+#include "chrono/utils/ChUtilsInputOutput.h"
+
+#include "chrono_vehicle/ChVehicleModelData.h"
+
+#include "chrono_thirdparty/filesystem/path.h"
+
+using namespace chrono;
+using namespace chrono::geometry;
+using namespace chrono::viper;
+
+bool output = true;
+const std::string out_dir = GetChronoOutputPath() + "SCM_DEF_SOIL";
+
+// SCM grid spacing
+double mesh_resolution = 0.02;
+
+// Enable/disable bulldozing effects
+bool enable_bulldozing = true;
+
+// Enable/disable moving patch feature
+bool enable_moving_patch = true;
+
+// If true, use provided callback to change soil properties based on location
+bool var_params = true;
+
+// Define Viper rover wheel type
+ViperWheelType wheel_type = ViperWheelType::RealWheel;
+
+// Use custom material for the Viper Wheel
+bool use_custom_mat = false;
+
+// Return customized wheel material parameters
+std::shared_ptr<ChMaterialSurface> CustomWheelMaterial(ChContactMethod contact_method) {
+    float mu = 0.4f;   // coefficient of friction
+    float cr = 0.1f;   // coefficient of restitution
+    float Y = 2e7f;    // Young's modulus
+    float nu = 0.3f;   // Poisson ratio
+    float kn = 2e5f;   // normal stiffness
+    float gn = 40.0f;  // normal viscous damping
+    float kt = 2e5f;   // tangential stiffness
+    float gt = 20.0f;  // tangential viscous damping
+
+    switch (contact_method) {
+        case ChContactMethod::NSC: {
+            auto matNSC = chrono_types::make_shared<ChMaterialSurfaceNSC>();
+            matNSC->SetFriction(mu);
+            matNSC->SetRestitution(cr);
+            return matNSC;
+        }
+        case ChContactMethod::SMC: {
+            auto matSMC = chrono_types::make_shared<ChMaterialSurfaceSMC>();
+            matSMC->SetFriction(mu);
+            matSMC->SetRestitution(cr);
+            matSMC->SetYoungModulus(Y);
+            matSMC->SetPoissonRatio(nu);
+            matSMC->SetKn(kn);
+            matSMC->SetGn(gn);
+            matSMC->SetKt(kt);
+            matSMC->SetGt(gt);
+            return matSMC;
+        }
+        default:
+            return std::shared_ptr<ChMaterialSurface>();
+    }
+}
+
+int main(int argc, char* argv[]) {
+    GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
+
+    auto vis_mesh_file = GetChronoDataFile(".");
+
+    std::cout << "Data dir is " << vis_mesh_file << std::endl;
+
+    // Global parameter for moving patch size:
+    double wheel_range = 0.5;
+    ////double body_range = 1.2;
+
+    // Create a Chrono::Engine physical system
+    ChSystemSMC sys;
+    sys.Set_G_acc(ChVector<>(0, 0, -9.81));
+
+    // Create the rover
+    auto driver = chrono_types::make_shared<ViperDCMotorControl>();
+
+    Viper viper(&sys, wheel_type);
+
+    viper.SetDriver(driver);
+    if (use_custom_mat)
+        viper.SetWheelContactMaterial(CustomWheelMaterial(ChContactMethod::NSC));
+
+    viper.Initialize(ChFrame<>(ChVector<>(-5, 0, -0.2), QUNIT));
+
+    // Get wheels and bodies to set up SCM patches
+    auto Wheel_1 = viper.GetWheel(ViperWheelID::V_LF)->GetBody();
+    auto Wheel_2 = viper.GetWheel(ViperWheelID::V_RF)->GetBody();
+    auto Wheel_3 = viper.GetWheel(ViperWheelID::V_LB)->GetBody();
+    auto Wheel_4 = viper.GetWheel(ViperWheelID::V_RB)->GetBody();
+    auto Body_1 = viper.GetChassis()->GetBody();
+
+    float t = 0.0;
+    while (t < 1.0) {
+
+        if (output) {
+            // std::cout << sys.GetChTime() << viper.GetWheelTracTorque(ViperWheelID::V_LF)
+            //     << viper.GetWheelTracTorque(ViperWheelID::V_RF) << viper.GetWheelTracTorque(ViperWheelID::V_LB)
+            //     << viper.GetWheelTracTorque(ViperWheelID::V_RB) << std::endl;
+            ChFrame<> body_ref_frame = Wheel_1->GetFrame_REF_to_abs();
+            ChVector<> body_pos = body_ref_frame.GetPos();  
+            std::cout << "Wheel pos: " << body_pos.x() << " " << body_pos.y() << " " << body_pos.z() << std::endl;
+        }
+
+        sys.DoStepDynamics(5e-4);
+        viper.Update();
+        t += 5e-4;
+        ////terrain.PrintStepStatistics(std::cout);
+    }
+
+    return 0;
+}
